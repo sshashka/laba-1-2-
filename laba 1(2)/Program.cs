3e03b854dc0931c3a121a7fc8d22e3f72c0f34cd
sshashka <63012816+sshashka@users.noreply.github.com>
2020-04-02 22:14:11 +0300

    Initial commit

---
+++ laba 1(2)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть чисельник та знаменник першого та другого дробу");
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int b1 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+            sas a = new sas(a1, a2);
+            sas b = new sas (b1, b2);
+            a.Sokr();
+            Console.WriteLine(a.a + " " + a.b);
+            b.Sokr();
+            Console.WriteLine(b.a + " " + b.b);
+            if (a.eqvi(a, b))
+                Console.WriteLine("eqv");
+            else
+                Console.WriteLine("n eqv");
+            sas c = new sas();
+            c.Dod(a, b);
+
+            Console.WriteLine(a.a + " " + a.b);
+            Console.WriteLine(b.a + " " + b.b);
+            sas d = new sas();
+            d.Dil(a, b);
+            Console.WriteLine(a.a + " " + a.b);
+            Console.WriteLine(b.a + " " + b.b);
+            sas e = new sas();
+            e.vidn(a, b);
+            Console.WriteLine(a.a + " " + a.b);
+            Console.WriteLine(b.a + " " + b.b);
+            sas f = new sas();
+            f.Mnoj(a, b);
+            Console.WriteLine(a.a + " " + a.b);
+            Console.WriteLine(b.a + " " + b.b);
+            Console.WriteLine(c.a + " " + c.b  + " Додавання");
+            Console.WriteLine(d.a + " " + d.b + " dil " );
+            Console.WriteLine(e.a + " " + e.b + " Vidn");
+            Console.WriteLine(f.a + " " + f.b + " Mnoj");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ laba 1(2)/sas.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_2_
+{
+    class sas
+    {
+        public int a;
+        public int b;
+        public void prav()
+        {
+            if (a < b)
+            {
+                Console.WriteLine("prav");
+            }
+            else
+            {
+                Console.WriteLine("NePrav");
+            }
+        }
+
+        private int normal(int p, int q)
+        {
+            int x, y, NOD;
+            x = Math.Abs(p);
+            y = Math.Abs(q);
+            while (Math.Abs(x) != Math.Abs(y))
+            {
+              
+                if (x > y)
+                    x = x - y;
+                else y = y - x;
+            }
+            NOD = x;
+            return x;
+        }
+
+        public void Sokr()
+        {
+            int j = normal(a, b);
+            a = a / j;
+            b = b / j;
+        }
+
+        public sas()
+        {
+
+        }
+
+        public sas(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public bool eqvi(sas d1, sas d2)
+        {
+            sas d3 = d1;
+            sas d4 = d2;
+            d4.Sokr();
+            d3.Sokr();
+            if (d3.a == d4.a && d3.b == d4.b)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Dod(sas d3, sas d4)
+        {
+            d4.Sokr();
+            d3.Sokr();
+            this.a = d3.a * d4.b + d4.a * d3.b;
+            this.b = d3.b * d4.b;             
+        }
+
+        public void vidn(sas d3, sas d4)
+        {
+            d4.Sokr();
+            d3.Sokr();
+            this.a = d3.a * d4.b - d4.a * d3.b;
+            this.b = d3.b * d4.b;   
+        }
+
+        public void Mnoj(sas d3, sas d4)
+        {
+            d4.Sokr();
+            d3.Sokr();
+            this.a = d3.a *d4.a;
+            this.b = d3.b * d4.b;   
+        }
+
+        public void Dil(sas d3, sas d4)
+        {
+            d4.Sokr();
+            d3.Sokr();
+            this.a = d3.a * d4.b;+
+            this.b = d4.a * d3.b;      
+        }
+    }
+}
